89465818b1cd643092233bbdf9e2c4be02c39e73
AmitJalal <amitjalal53@gmail.com>
2022-11-16 23:51:38 +0530

    introduced browser-router to the app and index.js

--- crown-clothing-v2/src/App.js
+++ crown-clothing-v2/src/App.js
@@ -1,36 +1,14 @@
-import Directory from "./components/directory/directory.component";
+import {Routes, Route} from 'react-router-dom'
+
+import Home from "./components/routes/home/home.component";
+
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "Sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "Women",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "Men",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
   return (
-   <Directory categories={categories}/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+
+    </Routes>
   );
 };
 
