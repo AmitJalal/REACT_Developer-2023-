1f82269a9ac5521fdd495424a2d30a9f4f797563
AmitJalal <amitjalal53@gmail.com>
2022-11-16 23:17:22 +0530

    some modifications and added react-router-dom v6

--- crown-clothing-v2/src/App.js
+++ crown-clothing-v2/src/App.js
@@ -4,27 +4,27 @@ const App = () => {
   const categories = [
     {
       id: 1,
-      title: "hats",
+      title: "Hats",
       imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     },
     {
       id: 2,
-      title: "jackets",
+      title: "Jackets",
       imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     },
     {
       id: 3,
-      title: "sneakers",
+      title: "Sneakers",
       imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
     },
     {
       id: 4,
-      title: "womens",
+      title: "Women",
       imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
     {
       id: 5,
-      title: "mens",
+      title: "Men",
       imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
